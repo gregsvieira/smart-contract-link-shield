56a9c24b8b555c502f2c6e0f6af29354c7a34a93
Gregory Vieira <vieiragregory42@gmail.com>
2025-08-05 18:17:09 -0300

    initial project

---
+++ smart-contract/link-shield.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+contract LinkShield {
+
+    struct Link {
+        string url;
+        address owner;
+        uint256 fee;
+    }
+
+   
+}
