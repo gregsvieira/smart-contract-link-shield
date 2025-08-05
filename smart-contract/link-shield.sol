4e2f3d9f40bddc5ea7f72ff78a3d36cdf8e51c9d
Gregory Vieira <vieiragregory42@gmail.com>
2025-08-05 18:19:29 -0300

    update: add link validation access

--- smart-contract/link-shield.sol
+++ smart-contract/link-shield.sol
@@ -10,17 +10,21 @@ contract LinkShield {
         uint256 fee;
     }
 
+    uint256 public commission = 1;
     mapping(string => Link) private links; 
+    mapping(string => mapping(address => bool)) public hasAccess;
 
     function addLink(string calldata url, string calldata linkId, uint256 fee) public {
         Link memory link = links[linkId];
         require(link.owner == address(0) || link.owner == msg.sender, "This linkId already has an owner");
+        require(fee == 0 || fee > commission, "Fee too low");
 
         link.url = url;
         link.fee = fee;
         link.owner = msg.sender;
 
         links[linkId] = link;
+        hasAccess[linkId][msg.sender] = true;
     }
 
 }
