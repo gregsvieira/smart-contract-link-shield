fd41064d5509ee895955ba633dca81dddab77bfe
Gregory Vieira <vieiragregory42@gmail.com>
2025-08-05 18:18:50 -0300

    feat: addLink fn

--- smart-contract/link-shield.sol
+++ smart-contract/link-shield.sol
@@ -10,5 +10,17 @@ contract LinkShield {
         uint256 fee;
     }
 
-   
+    mapping(string => Link) private links; 
+
+    function addLink(string calldata url, string calldata linkId, uint256 fee) public {
+        Link memory link = links[linkId];
+        require(link.owner == address(0) || link.owner == msg.sender, "This linkId already has an owner");
+
+        link.url = url;
+        link.fee = fee;
+        link.owner = msg.sender;
+
+        links[linkId] = link;
+    }
+
 }
